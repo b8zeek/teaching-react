fb437c5378a9c41e6d4a3f9d6da8b6ead6464306
Basic Mirko <bejzik8@gmail.com>
2022-08-24 20:04:19 +0100

    Added header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 import HomePage from './pages/HomePage'
@@ -7,6 +7,15 @@ function App() {
 
   return (
     <div className="App">
+      <div style={{
+        width: '100%',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        background: 'rgba(255, 255, 255, .1)'
+      }}>
+        <Link to=''>Home</Link>
+      </div>
       <Routes>
         <Route path='' element={<HomePage />} />
       </Routes>
